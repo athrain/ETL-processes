0bbffc2adb4b56a46b5eae660c4af50ac4d0b252
Dawid <dawidniewdana@gmail.com>
2023-06-20 14:18:40 +0200

    Further steps in analysis
    Adding a function at the beginning, then additional steps in analysis

--- Updating PostgreSQL DB - scripts/Interview details Table - Updating PostgreSQL DB.py
+++ Updating PostgreSQL DB - scripts/Interview details Table - Updating PostgreSQL DB.py
@@ -1,3 +1,12 @@
+def insert_into_table (values_to_insert):
+    with engine.connect() as conn:
+            conn.execute(sql
+                         .insert(table)
+                         .values
+                            ([values_to_insert])
+                     )
+            conn.commit()
+
 import pandas as pd
 import sqlalchemy as sql
 
@@ -65,3 +74,93 @@ raw_data = raw_data.drop(raw_data[raw_data['interview_stage']=='Candidate Summar
 raw_data = raw_data.drop(raw_data[raw_data['interview_stage']=='Phone Screen'].index)
 raw_data = raw_data.drop(raw_data[raw_data['interview_stage']=='IN Recruiter Phone Screen'].index)
 raw_data = raw_data.drop(raw_data[raw_data['interview_stage']==' '].index)
+
+###create a DF that stores information about the level of the interview and the progress, that we will use further in analysis
+
+d = {"progress_in_interviews_number": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "progress_in_interviews_names":
+     ['First Interview', 'Second Interview', 'Third Interview', 'Fourth Interview', 'Fifth Interview','Sixth Interview', 'Seventh Interview',
+     'Eighth Interview', 'Ninth Interview', 'Tenth Interview', 'Eleventh Interview', 'Twelfth Interview', 'Thirteenth Interview', 'Fourteenth Interview']}
+interview_stages_names_df = pd.DataFrame(data=d)
+
+###add completely new app ids with their interviews
+
+unique_values_main_table = pd.DataFrame(columns=['application_id'])
+uniques = main_table['application_id'].unique()
+unique_values_main_table['application_id'] = uniques.tolist()
+
+unique_values_raw_data = pd.DataFrame(columns=['application_id'])
+uniques = raw_data['application_id'].unique()
+unique_values_raw_data['application_id'] = uniques.tolist()
+
+new_values_df = pd.concat([unique_values_main_table,unique_values_raw_data]).drop_duplicates(keep=False) ###creates a unique values, that are not yet in the main table
+
+###delete app ids that no longer exist in raw data = meaning these candidates had their interviews canceled and never happened
+###we do so by simple left outer join merge in pandas - so that we have only values that exist in new values df, but are not present in raw data
+
+
+no_longer_existing_candidates_df = pd.merge(new_values_df, unique_values_raw_data, on=['application_id'], how="outer",
+                                            indicator=True)
+no_longer_existing_candidates_df = no_longer_existing_candidates_df[
+    no_longer_existing_candidates_df['_merge'] == 'left_only']
+
+print(f'The number of rows that we need to delete is: {len(no_longer_existing_candidates_df)}')
+
+for row in range(len(no_longer_existing_candidates_df)):
+    application_id = no_longer_existing_candidates_df.iloc[row, 0]
+    with engine.connect() as conn:
+        conn.execute(sql
+                     .delete(table)
+                     .where(table.c.application_id == application_id)
+                     )
+        conn.commit()
+    print(f'Row deleted: {row} !')
+
+    ###delete that apps also from new_values_df, so that they are not added to main table
+    new_values_df = new_values_df.drop(new_values_df[new_values_df['application_id'] == application_id].index)
+    new_values_df = new_values_df.reset_index()
+    new_values_df = new_values_df.drop(columns=['index'])  ###get rid of the unnecessary column in this table, created by reseting index
+
+###Create a loop to fill in the informations. Firstly, get the unique ID of the interview, then create ad hoc table with interviews for one candidate, based on App ID
+###Later on, sort the table in one candidate by Interview Time, so that we could check which one was the last one, which one was the first one etc.
+###Then create another for loop within the one candidate table and check how many interviews this person had and add necessary data
+###Lastly, add that to PostgreSQL DB
+
+for counter in range(len(new_values_df)):
+
+    application_id = new_values_df.iloc[counter, 0]
+
+    one_candidate_interviews_df = raw_data[raw_data['application_id'] == application_id]
+    one_candidate_interviews_df = one_candidate_interviews_df.sort_values(by='interview_time', ascending=True)
+    one_candidate_interviews_df = one_candidate_interviews_df.reset_index()
+    one_candidate_interviews_df = one_candidate_interviews_df.drop(
+        columns=['index'])  ###get rid of the unnecessary column in this table, created by reseting index
+
+    counter_plus = 0
+
+    for counter_plus in range(len(one_candidate_interviews_df)):
+
+        try:
+            one_candidate_interviews_df.at[counter_plus, "progress_in_interviews_names"] = \
+            interview_stages_names_df.iloc[counter_plus, 1]
+            one_candidate_interviews_df.at[counter_plus, "progress_in_interviews_number"] = str(
+                interview_stages_names_df.iloc[counter_plus, 0])
+        except:
+            print('Out of range, more than 14 interviews!')
+            pass
+        counter_plus += 1
+
+    if len(one_candidate_interviews_df) == 1:
+        one_candidate_interviews_df.at[0, "first_last_interview"] = "Only one interview"
+    else:
+        one_candidate_interviews_df.at[0, "first_last_interview"] = "First interview"
+        one_candidate_interviews_df.at[len(one_candidate_interviews_df) - 1, "first_last_interview"] = "Last interview"
+
+    print(one_candidate_interviews_df)
+    for dataframe in range(len(one_candidate_interviews_df)):
+        values_in_dict = one_candidate_interviews_df.loc[dataframe].to_dict()
+        # print(one_candidate_interviews_df)
+        print(values_in_dict)
+        print('We are here')
+        insert_into_table(values_in_dict)
+
+    counter += 1
