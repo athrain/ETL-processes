87bbe119dd91401f9e9c7c2b1eb5cd4434e52da0
Dawid <dawidniewdana@gmail.com>
2023-07-02 16:23:22 +0200

    Pseudocode - Offer Hire Details Table - Update
    Pseudo code

---
+++ Updating PostgreSQL DB - scripts/Offer Hire Detail Table - Updating PostgreSQL DB.py
@@ -0,0 +1,19 @@
+###It's still a pseudo code - some ideas to work on later on
+
+
+###Step 1
+#Connect to database, import Excel file
+
+###Step 2
+#Connect to a table in PostgreSQL (without creating it once again)
+
+###Step 3
+#Finding new, unique offers
+#Firstly, get new unique IDs of applications by comparing both tables
+#Then, add it to the table
+
+###Step 4
+#Compare the offers that were made previously
+#compare 2 dictionaries - if they are the same, then skip
+#if not - upload a dictionary from the raw data
+
