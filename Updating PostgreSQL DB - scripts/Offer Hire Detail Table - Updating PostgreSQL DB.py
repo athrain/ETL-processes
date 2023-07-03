43968b8dbfa13e6a49c95437beef861070c116c8
Dawid <dawidniewdana@gmail.com>
2023-07-03 08:33:36 +0200

    Add one additional step - duplicates

--- Updating PostgreSQL DB - scripts/Offer Hire Detail Table - Updating PostgreSQL DB.py
+++ Updating PostgreSQL DB - scripts/Offer Hire Detail Table - Updating PostgreSQL DB.py
@@ -7,6 +7,9 @@
 ###Step 2
 #Connect to a table in PostgreSQL (without creating it once again)
 
+###Step
+#Replace duplicates in raw data
+
 ###Step 3
 #Finding new, unique offers
 #Firstly, get new unique IDs of applications by comparing both tables
