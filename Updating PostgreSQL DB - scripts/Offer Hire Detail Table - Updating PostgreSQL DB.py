397ca080efae21f48633926e0d98be5ec28a93bf
Dawid <dawidniewdana@gmail.com>
2023-07-04 16:51:59 +0200

    Update Offer Hire Detail Table - Updating PostgreSQL DB.py
    Adding step 1 and step 2

--- Updating PostgreSQL DB - scripts/Offer Hire Detail Table - Updating PostgreSQL DB.py
+++ Updating PostgreSQL DB - scripts/Offer Hire Detail Table - Updating PostgreSQL DB.py
@@ -4,8 +4,42 @@
 ###Step 1
 #Connect to database, import Excel file
 
+import pandas as pd
+import sqlalchemy as sql
+
+###load all the tables into the script - main table from PostgreSQL DB and raw data
+
+engine = sql.create_engine('postgresql+psycopg2://USERNAME:PASSWORD@localhost/DATABASENAME')
+main_table = pd.read_sql("offer_hire_details", con=engine.connect())
+duplicate_values = pd.read_excel(r'C:\filepath\filename.xlsx')  ###excel with duplicate values to replace
+raw_data = pd.read_excel(r'C:\filepath\filename.xlsx') ###new values to compare with the main table
+
+print (f'Connections to PostgreSQL Table was established and all the tables were converted into dataframes')
+
 ###Step 2
 #Connect to a table in PostgreSQL (without creating it once again)
+
+metadata = sql.MetaData()
+
+table_name = "offer_hire_details"
+
+table = sql.Table(
+    table_name,
+    metadata,
+    sql.Column("application_id", sql.String),
+    sql.Column("candidate_id", sql.String),
+    sql.Column("job_title_name", sql.String),
+    sql.Column("form_of_cooperation", sql.String),
+    sql.Column("salary", sql.String),
+    sql.Column("hired_date", sql.DateTime),
+    sql.Column("start_date", sql.DateTime),
+    sql.Column("hiring_entity", sql.String),
+    sql.Column("work_model", sql.String),
+    sql.Column("supervisory_organisation", sql.String),
+    sql.Column("direct_supervisor", sql.String)
+)
+
+print (f'A table {table_name} was succesfully prepared')
 
 ###Step
 #Replace duplicates in raw data
