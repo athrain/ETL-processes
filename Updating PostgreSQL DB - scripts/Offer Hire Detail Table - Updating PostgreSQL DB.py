de3dcee839c2807e7886f8f6c4ea7253b6cf5c02
Dawid <dawidniewdana@gmail.com>
2023-07-04 17:03:43 +0200

    Function to add new offers
    Adding a function to add new offers

--- Updating PostgreSQL DB - scripts/Offer Hire Detail Table - Updating PostgreSQL DB.py
+++ Updating PostgreSQL DB - scripts/Offer Hire Detail Table - Updating PostgreSQL DB.py
@@ -44,10 +44,52 @@ print (f'A table {table_name} was succesfully prepared')
 ###Step
 #Replace duplicates in raw data
 
+for row in range(len(duplicate_values)):
+
+    looked_duplicate_id = duplicate_values['Original_Value'].loc[row]
+
+    if duplicate_values.loc[duplicate_values['Original_Value'] == looked_duplicate_id, 'To_Replace_With'].iloc[0] == "DONOTCHANGE":
+        pass
+    else:
+        try:
+            position_to_replace = raw_data[raw_data['Application Encrypted ID'] == looked_duplicate_id].index.values
+            replaced_value = duplicate_values.loc[duplicate_values['Original_Value'] == looked_duplicate_id, 'To_Replace_With'].iloc[0]
+            raw_data.at[int(position_to_replace), 'Application Encrypted ID'] = replaced_value
+        except:
+            pass
+        ###it passess if it does not find a duplicate value in a data table
+
+
 ###Step 3
 #Finding new, unique offers
 #Firstly, get new unique IDs of applications by comparing both tables
 #Then, add it to the table
+
+raw_data = raw_data.rename(columns={"Application Encrypted ID": "application_id"})
+
+
+main_table_to_compare = main_table.loc["application_id"]
+raw_data_to_compare = raw_data.loc["application_id"]
+new_values_df = pd.concat([main_table_to_compare,raw_data_to_compare]).drop_duplicates(keep=False) ###creates a unique values, that are not yet in the main table
+
+###create unique values from new import, to prepare the data for upload to the system
+unique_values_df = pd.DataFrame(columns=['application_id'])
+uniques = new_values_df['application_id'].unique()
+unique_values_df['application_id'] = uniques.tolist()
+
+###add brand new offers to the PostgreSQL table
+
+for row in range (len(unique_values_df)):
+
+    application_id = unique_values_df['application_id'].loc[row]
+    values_in_dict = raw_data[raw_data['application_id'] == application_id].to_dict()
+
+    with engine.connect() as conn:
+        conn.execute(sql
+                    .insert(table)
+                    .values([values_in_dict])
+                    )
+        conn.commit()
 
 ###Step 4
 #Compare the offers that were made previously
